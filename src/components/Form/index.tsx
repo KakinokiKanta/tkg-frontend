9a3ad8e7cb5f57dac66373c52994f3ad467110d3
KakinokiKanta <kanta12621@icloud.com>
2024-01-26 03:39:54 +0900

    add: フォームに入力した値をconsoleに表示する処理を追加

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,18 +1,34 @@
 "use client";
 
+import { useForm, SubmitHandler } from "react-hook-form";
 import styles from "./Form.module.scss";
 
+type Inputs = {
+  id: number;
+  password: string;
+};
+
 export const Form = () => {
-  const handleSubmit = async () => {
-    const result = await console.log("login!");
-  };
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <p>ID : </p>
-
-      <p>パスワード : </p>
-      <button>ログイン</button>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID</label>
+        <input id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">パスワード</label>
+        <input id="password" {...register("password")} type="password" />
+      </div>
+      <button type="submit">ログイン</button>
     </form>
   );
 };
