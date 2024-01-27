f2510ffd4da844cb78b50788c87b4ea8170018b7
KakinokiKanta <kanta12621@icloud.com>
2024-01-28 00:53:30 +0900

    update: ディレクトリ構成を変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+import { CharacterImg } from "./_components/CharacterImg";
 
-export default function Home() {
+const Character = () => {
+  // キャラ移動の方向を決める関数
+  const getRandomDirection = () => {
+    const angle = Math.random() * Math.PI * 2;
+    return { x: Math.cos(angle), y: Math.sin(angle) };
+  };
+
+  // キャラ移動の速度を決める関数
+  const getRandomSpeed = () => {
+    return Math.random() * 2 + 1;
+  };
+
   return (
-    <main className={styles.main}>
-      <div>Hello World!!</div>
-    </main>
+    <div className={styles.window}>
+      <div className={styles.frame}>
+        <h1 className={styles.title}>TKG World!!</h1>
+      </div>
+      <CharacterImg />
+    </div>
   );
-}
+};
+
+export default Character;
