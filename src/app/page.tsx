3d0661c3682acc2af5943fb8c72cda7cbe4fdb41
KakinokiKanta <kanta12621@icloud.com>
2024-01-29 21:32:46 +0900

    delete: トップページを初期化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,9 @@
 import styles from "./page.module.scss";
-import { CharacterImg } from "./_components/CharacterImg";
 
 const Character = () => {
-  // キャラ移動の方向を決める関数
-  const getRandomDirection = () => {
-    const angle = Math.random() * Math.PI * 2;
-    return { x: Math.cos(angle), y: Math.sin(angle) };
-  };
-
-  // キャラ移動の速度を決める関数
-  const getRandomSpeed = () => {
-    return Math.random() * 2 + 1;
-  };
-
   return (
     <div className={styles.window}>
-      <CharacterImg />
+      <p>Hello World!!</p>
     </div>
   );
 };
