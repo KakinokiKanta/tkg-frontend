48f069d2f8290ec58339224d354a547a0a26c7b4
KakinokiKanta <kanta12621@icloud.com>
2024-01-29 23:34:03 +0900

    add: トップページの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
+import Image from "next/image";
 import styles from "./page.module.scss";
+import Link from "next/link";
 
-const Character = () => {
+const Top = () => {
   return (
     <div className={styles.window}>
-      <p>Hello World!!</p>
+      <div className={styles.titleFrame}>
+        <Image
+          src={"/tkg_logo.png"}
+          alt="TKGのロゴ"
+          fill
+          style={{ objectFit: "cover" }}
+          className={styles.titleImg}
+        />
+      </div>
+      <div className={styles.grid}>
+        <Link href={"/character"} className={styles.card}>
+          育成
+        </Link>
+        <Link href={"/"} className={styles.card}>
+          コレクション
+        </Link>
+        <Link href={"/"} className={styles.card}>
+          ランキング
+        </Link>
+      </div>
     </div>
   );
 };
 
-export default Character;
+export default Top;
