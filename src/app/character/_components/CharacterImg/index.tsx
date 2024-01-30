17b185bd4b61c5da2f7b96dda6b4f20c4159d7c0
KakinokiKanta <kanta12621@icloud.com>
2024-01-31 03:00:30 +0900

    update: ぽこべえが動くようにした(requestAnimationFrameを使用した処理に書き換えるかも)

--- src/app/character/_components/CharacterImg/index.tsx
+++ src/app/character/_components/CharacterImg/index.tsx
@@ -1,15 +1,41 @@
 "use client";
 
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "./CharacterImg.module.scss";
 
 export const CharacterImg = () => {
+  const [position, setPosition] = useState({ x: 50, y: 50 });
+  const X_MAX = 5;
+  const Y_MAX = 5;
+
+  // キャラ移動の方向を決める関数
+  const getRandomDirection = () => {
+    const xDirection = Math.random() * X_MAX * 2 - X_MAX;
+    const yDirection = Math.random() * Y_MAX * 2 - Y_MAX;
+    return { x: xDirection, y: yDirection };
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPosition({
+        x: position.x + getRandomDirection().x,
+        y: position.y + getRandomDirection().y,
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>
+    <div
+      className={styles.move}
+      style={{ top: `${position.x}%`, left: `${position.y}%` }}
+    >
       <Image
         src={"/character/pokobe-pix.png"}
-        width={300}
-        height={250}
+        width={200}
+        height={160}
         alt="キャラクター画像"
         className={styles.img}
       />
