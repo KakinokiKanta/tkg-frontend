13698062c062953dfce46bbd3802a1f5998f87b7
KakinokiKanta <kanta12621@icloud.com>
2024-01-31 18:05:35 +0900

    fix: リファクタリング

--- src/app/character/_components/CharacterImg/index.tsx
+++ src/app/character/_components/CharacterImg/index.tsx
@@ -7,28 +7,6 @@ import { useAnimationFrame } from "../../_hooks/useAnimationFrame";
 
 export const CharacterImg = () => {
   const position = useAnimationFrame();
-  // const [position, setPosition] = useState({ x: 50, y: 50 });
-  // const X_MAX = 5;
-  // const Y_MAX = 5;
-
-  // // キャラ移動の方向を決める関数
-  // const getRandomDirection = () => {
-  //   const xDirection = Math.random() * X_MAX * 2 - X_MAX;
-  //   const yDirection = Math.random() * Y_MAX * 2 - Y_MAX;
-  //   return { x: xDirection, y: yDirection };
-  // };
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setPosition((position) => {
-  //       return {
-  //         x: position.x + getRandomDirection().x,
-  //         y: position.y + getRandomDirection().y,
-  //       };
-  //     });
-  //   }, 1000);
-  //   // return () => clearInterval(interval);
-  // }, []);
 
   return (
     <div
