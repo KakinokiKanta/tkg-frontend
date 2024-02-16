1cd08f882bae1686c1fc97e57b5ce6b506b4a6e3
KakinokiKanta <kanta12621@icloud.com>
2024-02-17 01:55:35 +0900

    delete: いらない処理を削除

--- src/app/(authentication)/login/_components/GithubOAuth/index.tsx
+++ src/app/(authentication)/login/_components/GithubOAuth/index.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import styles from "./GithubOAuth.module.scss";
 import Image from "next/image";
 import { signIn } from "next-auth/react";
 
 export const GithubOAuth = () => {
-  const router = useRouter();
-
   const handleClick = async () => {
     // Githubで認証
     const result = await signIn("github");
